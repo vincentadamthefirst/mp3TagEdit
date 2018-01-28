444d3642aa224d034a4ff6e47ad1afac8ee7e540
Vincent <33129919+ItsTheJetstream@users.noreply.github.com>
2018-01-28 23:46:26 +0100

    Update 28-01-18 Vincent
    Changed App Logo (now more minimalistic) Transferred Code from old Activities to the newer ones

--- app/src/main/java/mp3tagedit/de/main/id3v24editor.java
+++ app/src/main/java/mp3tagedit/de/main/id3v24editor.java
@@ -1,10 +1,15 @@
 package mp3tagedit.de.main;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Point;
+import android.media.MediaPlayer;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
@@ -28,8 +33,13 @@ import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
+import org.jaudiotagger.tag.id3.reference.MediaPlayerRating;
+
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class id3v24editor extends AppCompatActivity {
 
@@ -37,9 +47,19 @@ public class id3v24editor extends AppCompatActivity {
 
     private final static int PERM_REQ_WRITE_STORAGE = 42;
     private final static int PERM_REQ_READ_STORAGE = 43;
+    private final static int PERM_REQ_CAMERA = 44;
 
     public static ArrayList<EditText> artistList;
     public static ArrayList<EditText> genreList;
+
+    MediaPlayer mediaPlayer;
+    private int AudioSession = 1;
+    private int currentPos;
+
+    private Button playButton;
+
+    // TODO
+    File currentFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,14 +67,34 @@ public class id3v24editor extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_id3v24editor);
 
-        artistList = new ArrayList<EditText>();
+        artistList = new ArrayList<>();
         artistList.add((EditText)(findViewById(R.id.artistIn)));
-
-        genreList = new ArrayList<EditText>();
+        genreList = new ArrayList<>();
         genreList.add((EditText)(findViewById(R.id.genreIn)));
 
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERM_REQ_WRITE_STORAGE);
 
-        //if you want to update the items at a later time it is recommended to keep it in a variable
+        File[] allRootPaths = getExternalFilesDirs(null);
+        for (File f : allRootPaths) {
+            System.out.println(f.getAbsolutePath());
+            ArrayList<String> playL = getPlayList(f.getAbsolutePath().replace("Android/data/thejetstream.de.mp3tagedit/files", ""), ".mp3");
+            for (String s : playL) {
+                System.out.println(s);
+                if (s.contains("schlawinerwiener")) {
+                    currentFile = new File(s);
+                    System.out.println("GEFUNDEN !!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                    break;
+                }
+            }
+        }
+
+        setupDrawer();
+        setupActionBar(true, true, "id3v2.4 Editor");
+        setupEditorHead();
+
+    }
+
+    public void setupDrawer() {
         PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("Home");
         SecondaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2).withIcon(FontAwesome.Icon.faw_search).withName("Settings");
 
@@ -81,9 +121,45 @@ public class id3v24editor extends AppCompatActivity {
                         }
                     }
                 }).withDrawerWidthDp(240).build();
+    }
 
-        setupActionBar(true, true, "id3v2.4 Editor");
-        setupEditorHead();
+    /**
+     * Overrides existing Method
+     * Used to check if a Permission was granted by the user during runtime
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERM_REQ_WRITE_STORAGE: {
+                if (grantResults.length > 0) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                        // denied
+                        System.out.println("DENIED");
+                    } else {
+                        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                            // granted
+                            System.out.println("GRANTED");
+                        } else {
+                            // not clicked
+                            System.out.println("NOT CLICKED");
+                        }
+                    }
+                }
+            } case PERM_REQ_READ_STORAGE: {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    // denied
+                    System.out.println("DENIED");
+                } else {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        // granted
+                        System.out.println("GRANTED");
+                    } else {
+                        // not clicked
+                        System.out.println("NOT CLICKED");
+                    }
+                }
+            }
+        }
     }
 
     public void addInputLineArtist(View view){
@@ -179,13 +255,98 @@ public class id3v24editor extends AppCompatActivity {
     }
 
     private void setupEditorHead() {
-        Button playButton = findViewById(R.id.play_button);
+        playButton = findViewById(R.id.play_button);
         Button saveButton = findViewById(R.id.save_button);
         Button shareButton = findViewById(R.id.share_button);
 
-        playButton.setBackgroundDrawable(new IconicsDrawable(this).icon(GoogleMaterial.Icon.gmd_play_circle_outline).sizeDp(20).color(getResources().getColor(R.color.colorPrimary)));
-        shareButton.setBackgroundDrawable(new IconicsDrawable(this).icon(GoogleMaterial.Icon.gmd_share).sizeDp(20).color(getResources().getColor(R.color.colorPrimary)));
+        playButton.setBackgroundDrawable(new IconicsDrawable(this)
+                .icon(GoogleMaterial.Icon.gmd_play_arrow).sizeDp(20)
+                .color(getResources().getColor(R.color.colorPrimary)));
+        shareButton.setBackgroundDrawable(new IconicsDrawable(this)
+                .icon(GoogleMaterial.Icon.gmd_share).sizeDp(20)
+                .color(getResources().getColor(R.color.colorPrimary)));
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if (!mediaPlayer.isPlaying()) {
+                        playButton.setBackgroundDrawable(new IconicsDrawable(id3v24editor.this)
+                                .icon(GoogleMaterial.Icon.gmd_pause).sizeDp(20)
+                                .color(getResources().getColor(R.color.colorPrimary)));
+                        try {
+                            playSong();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        playButton.setBackgroundDrawable(new IconicsDrawable(id3v24editor.this)
+                                .icon(GoogleMaterial.Icon.gmd_play_arrow).sizeDp(20)
+                                .color(getResources().getColor(R.color.colorPrimary)));
+                        pauseSong();
+                    }
+                } catch (NullPointerException e) {
+                    playButton.setBackgroundDrawable(new IconicsDrawable(id3v24editor.this)
+                            .icon(GoogleMaterial.Icon.gmd_pause).sizeDp(20)
+                            .color(getResources().getColor(R.color.colorPrimary)));
+                    try {
+                        playSong();
+                    } catch (IOException e2) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
 
         saveButton.setText("TEST");
+    }
+
+    private void playSong() throws IOException {
+        if (mediaPlayer == null) {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(currentFile.getAbsolutePath());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } else {
+            mediaPlayer.seekTo(currentPos);
+            mediaPlayer.start();
+        }
+    }
+
+    private void pauseSong(){
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            currentPos = mediaPlayer.getCurrentPosition();
+        }
+    }
+
+    /**
+     * Returns a list of Hashmaps containing the path and name of every file with a certain extension.
+     * @param rootPath The root directory which should be searched
+     * @param fileExtension The extension the to be found files should have
+     * @return null if the file is not a directory or a list of Hashmaps
+     */
+    private ArrayList<String> getPlayList(String rootPath, String fileExtension) {
+        ArrayList<String> fileList = new ArrayList<>();
+
+        try {
+            File rootFolder = new File(rootPath);
+            File[] files = rootFolder.listFiles();
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    ArrayList<String> tmp = getPlayList(file.getAbsolutePath(), fileExtension);
+                    if (tmp != null) {
+                        fileList.addAll(tmp);
+                    } else {
+                        break;
+                    }
+                } else if (file.getName().endsWith(fileExtension)) {
+                    fileList.add(file.getAbsolutePath());
+                }
+            }
+            return fileList;
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
